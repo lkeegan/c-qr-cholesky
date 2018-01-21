7accb46a870963a91a533aab796beb400eb26fab
lkeegan <liam@keegan.ch>
2018-01-21 21:01:31 +0100

    c99 flag

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 g++ qr.cpp -shared -Wall -fPIC -Wno-int-in-bool-context -O3 -o libqr.so
 
 #compile c example program
-gcc example.c -L. -lqr -o example
+gcc -std=c99 example.c -L. -lqr -o example
 
 #run c example program
 LD_LIBRARY_PATH=. ./example
