8f923fb5bcf8f923374ec89739241dc058c06d35
lkeegan <liam@keegan.ch>
2018-01-21 21:12:34 +0100

    compile options

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
+#specify compilers:
+export CXX=g++
+export CC=gcc
+
 #compile qr library:
-g++ qr.cpp -shared -Wall -fPIC -Wno-int-in-bool-context -O3 -o libqr.so
+$CXX qr.cpp -shared -Wall -fPIC -Wno-int-in-bool-context -O3 -o libqr.so
 
 #compile c example program
-gcc -std=c99 example.c -L. -lqr -o example
+$CC -std=c99 -Wall example.c -L. -lqr -o example
 
 #run c example program
 LD_LIBRARY_PATH=. ./example
