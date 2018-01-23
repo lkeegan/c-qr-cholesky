80bd0b8ed628dffcb35ccc33f7e13f32eda94f45
lkeegan <liam@keegan.ch>
2018-01-23 10:18:59 +0100

    compiles with clang now

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #specify compilers if needed:
-#export CXX=g++
-#export CC=gcc
+#export CXX=clang++
+#export CC=clang
 
 #compile qr library:
 $CXX qr.cpp -shared -Wall -fPIC -Wno-int-in-bool-context -O3 -o libqr.so
 
 #compile c example program
-$CC -std=c99 -Wall -O3 example.c -L. -lqr -o example
+$CC -std=c99 -Wall -O3 -lm example.c -L. -lqr -o example
 
 #run c example program
 LD_LIBRARY_PATH=. ./example
